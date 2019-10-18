3925a745b854a04a7cde007838c341ba515947c5
Linerre <leon.errelin@outlook.com>
2019-10-18 13:09:25 +0800

    Happened to have created an infinite loop

--- cal_emu.py
+++ cal_emu.py
@@ -2,34 +2,37 @@
 # -*- coding: utf-8 -*-
 
 # let's say user does enter a number
-user_raw = float(input('Please enter a number: ')) # num_1
+num_one = float(input('Please enter a number: ')) # num_1
 
  # let's say we only support + - x /
-oper_raw = input('Please enter the operator: ')) # op_1
+op = input('Please enter the operator: ') # op_1
 
-while oper_raw != '+' or oper_raw != '-' or oper_raw != 'x' or oper_raw != '/':
+while op == '+' or op == '-' or op == '*' or op == '/':
     print('Operators only support +, -, *, /')
-    oper_raw = input('Please enter the operator: '))
+    op = input('Please enter the operator: ')
 
-user_one = float(input('Please enter another number: '))
+num_two = float(input('Please enter another number: ')) # num_2
+result = eval(num_one + op + num_two)
 
 while True:
-    oper_one = input('Please enter the operator or hit = to get the result')
-    if oper_one == '=':
-        result = eval(user_raw + oper_raw + user_one)
+    op = input('Please enter the operator or hit = to get the result')
+    if op == '=':
         print(result)
         break
     else:
-        user_two = float(input('Please enter still another number: '))
-        oper_two = 
-        result = eval(user_raw + oper_raw + user_one + )
+        num_one = result
+        num_two = float(input('Please enter still another number: '))
+        op = input('Please enter the operator')
+        result = eval(num_one + op + num_two)
+        continue
+
+
+
 
 # Try to calculate each time after you get two numbers
 
-# I tried to emulate a caculator that can at least carry out 
-# addition, deduction, multiple and division.
-# Yet this approach is severely flawed since you will need 
-# numberless vars to hold all the values, both operators and numbers
-# before you finally evaluate the caculation
-# I chose so because I wanted to see how far I can go without any use of list/dict.
-# I even tried to avoid eval(), which is almost impossible in Python. 
+# try this
+# (3 + 4) / 5 * 6
+# since this simple calculator just takes nums one by one 
+# it will do the cal from right to left
+# I use round brackets here for human reading purpoeses
