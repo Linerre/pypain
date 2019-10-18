fd03babef99b693aadd453d1f4473e07723694eb
Linerre <leon.errelin@outlook.com>
2019-10-18 15:27:44 +0800

    started working yet needs improvements

--- cal_emu.py
+++ cal_emu.py
@@ -17,17 +17,16 @@ result = str(eval(num_one + op + num_two)) # keep result a str for next eval if 
 # not a string! Pay detailed attention to this!
 
 while True:
-    op = input('Please enter the operator or hit = to get the result: ')
+    op = input('Please enter an operator or hit = to get the result: ') # continue or end?
+    while op != '+' and op != '-' and op != '*' and op != '/' and op != '=': # go on and check it
+            print('Operator must be one of +, -, *, /, =')
+            op = input('Please enter the operator: ') # already an op here!
     if op == '=':
         print(result)
         break
     else:
         num_one = result # integrate the previous result into the new counting base
         num_two = input('Please enter still another number: ') # ask for a new number
-        op = input('Please enter the operator: ') # ask for a new op
-        while op != '+' and op != '-' and op != '*' and op != '/': # check it
-            print('Operators only support +, -, *, /')
-            op = input('Please enter the operator: ')
         result = str(eval(num_one + op + num_two)) # here we go
         continue # start another round of loop
 
@@ -42,6 +41,7 @@ while True:
 # it will do the cal from right to left
 # I use round brackets here for human reading purpoeses
 
-# won't work after the 3rd number
-# keep looping
-# need to find out why
+
+# 8.399999999999999
+# it should be 8.4
+# Need to find out why
