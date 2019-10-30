14c19b084f185fcecdd92c6ac3c17ad711c881b8
Linerre <leon.errelin@outlook.com>
2019-10-30 18:18:29 +0800

    fixed the bug. Can improve later

--- encryption.py
+++ encryption.py
@@ -36,22 +36,18 @@ def alphabet(str):
 alphabet(lower+upper)
 
 for letter in source:
-    for card in deck: # wrong! since each letter will get 13 card!
-        cipher.append([letter, card])
+        cipher.append([letter, deck[source.index(letter)]]) #fix the bug, awkwardly
 
 encrypt = dict(cipher).update(punc)
 print(encrypt)
 
 # ==================== decipher ===================
-message = '''This takes a string, finds all occurrences of a number followed
-by an alphanu-meric word, and returns a string wherein every such occurrence
-is decrementedby one.'''
+message = 'This takes a string, finds all occurrences of a number followed by an alphanumeric word, and returns a string wherein every such occurrence is decrementedby one.'
 
-def enigma(str)
+def enigma(str):
     encrypted = []
-    for i in message:
-        if i in encrypt.keys():
-            encrypted.append(encrypt[i])
+    for i in str:
+        encrypted.append(encrypt[i])
     return ''.join(encrypted)
 
-print(enigam(message))
+print(enigma(message))
