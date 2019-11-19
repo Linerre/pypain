afb660cafa0c07c9cfdb287747c0b0d8681671f3
Linerre <leon.errelin@outlook.com>
2019-11-19 23:46:24 +0800

    get ready?

--- separator/sep.py
+++ separator/sep.py
@@ -6,22 +6,25 @@ import os
 from sys import argv
 script, filename = argv
 
-cwd = os.getcwd() # getting the working dir path
+cwd = os.getcwd()
 origin = 'para'   # the transcripts folder
 subt   = 'line'   # the subtitles folder
+filename = 'testtext.txt'
 
 cn_marks = r'，|：|“|”|·|、|——'
 
 # open the original files in para dir
-# separate lines
-# put new content in a file with the same name but into a different dir line
+# separate the paras (en-cn) to lines (cn - en)
+# put the new content in a file with the same name
+# but into a different dir line
 
-with open(filename, 'r+', encoding='utf-8', errors = 'ignore') as f:
+with open(os.path.join(cwd, origin, filename), 'r+', encoding='utf-8', errors = 'ignore') as f:
     content = f.read()
     cn_swap = re.sub(cn_marks, ' ', content)
     cn_sent = re.findall('[\u4e00-\u9fff0-9A-Za-z( )\[\]]+[。？!]', cn_swap)
     # “\u4e00-\u9fff0-9A-Za-z( )” can get strings with quotes like “你好”
 
+    # replace cn punctuations with en ones
     swap = content.replace('." ', '."\n')
     p_swap = swap.replace('. ', '.\n')
     q_swap = p_swap.replace('? ', '?\n')
@@ -30,8 +33,9 @@ with open(filename, 'r+', encoding='utf-8', errors = 'ignore') as f:
 
     cn_en = list(zip(cn_sent, en_sent))
 
+with open(os.path.join(cwd, origin, filename), 'w', encoding='utf-8', errors = 'ignore') as fnew:
     for cn, en in cn_en:
-        f.write(en+'\n'+cn+'\n')
+        fnew.write(cn+'\n'+en+'\n')
 
 print('cn_sent contains: ', len(cn_sent), 'sentences.')
 print('en_sent contains: ', len(en_sent), 'sentences.')
