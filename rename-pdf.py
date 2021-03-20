d6e0bfa8029ef7bc0eec9da94ffa5b9d4eecb382
Linerre <leon.errelin@outlook.com>
2021-03-20 23:30:54 +0800

    add: pdf-renaming script

---
+++ rename-pdf.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+
+"""
+This script will rename chapter PDFs in a batch
+within the same folder. It requires one command
+line argv, which is the barcode of the book.
+Input: 
+    <scriptname> <barcode> <book_title>
+Output: 
+    <barcode>_chapter_X.pdf
+    <barcode>_<book_title>  # dir name also changed
+"""
+
+import sys
+import os
+import getpass
+
+# root dir
+user = getpass.getuser()
+parent = f'C:\\Users\\{user}\\Desktop\\CDL\\'
+print(f'Changes will be made to {parent}\'s sub dir.')
+
+# parent = 'C:\\Users\\user\\Desktop\\CDL\\'
+
+# get barcode and book title from cmd line
+barcode = str(sys.argv[1])
+title = str(sys.argv[2])
+prefix = barcode + '_'
+
+try:
+    # enter CDL dir
+    os.chdir(parent)
+    
+    # first change book dir name
+    os.rename(title, prefix + title)
+
+    # update title 
+    title = prefix + title
+    print('Book tile dir renamed to: ', title)
+    print()
+    # enter the book dir
+    os.chdir(parent + title)
+    print(f'Entered {title} dir')
+    print()
+    # get all the chapters in an unordred list
+    chapters = os.listdir()
+    print('Got all chapter names: ')
+    print(chapters)
+    print()
+
+    # loop through them and modify their names 
+    print('Start renameing chapters ...')
+    for chapter in chapters:
+        os.rename(chapter, prefix + chapter)
+
+    print('Renaming done!', 'Going back to parent dir ...')
+
+    # cd to the parent dir
+    os.chdir('..\\')
+    print('JOB DONE')
+except:
+    print('Oops, sth wrong!')
+
