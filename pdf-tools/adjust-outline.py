2476f3fb6ceb4d3878c53e56e78a7e5258680554
Linerre <leon.errelin@outlook.com>
2021-06-04 23:47:05 +0800

    add: outline adjuster

---
+++ pdf-tools/adjust-outline.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+This script will adjust a PDF file's real pages
+according to its TOC.
+Real pages start at 1 and end at a positive number >= 1.
+e.g. page xii may be converted to a real page as page 23.
+"""
+
