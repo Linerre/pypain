e43ce4ec8c9d7485e15512f90e67fedcd3b54ff5
Linerre <leon.errelin@outlook.com>
2023-04-25 02:45:58 +0800

    Add git, hugo, java, clojure, etc

--- fs-tools/version.sh
+++ fs-tools/version.sh
@@ -3,9 +3,6 @@
 # Ask me about the `version' of any given argument
 #
 
-
-# echo "The version of $1 is: "
-
 # Check if cmd exists (installed)
 _check_cmd() {
     local cmd=$1
@@ -19,7 +16,7 @@ _check_cmd() {
 
 _check_cmd $1
 
-
+# If it exits, print its `version' usage and then its current version info
 print_version() {
     local cmd=$(basename $1)
     case $cmd in
@@ -36,22 +33,51 @@ print_version() {
             echo "------------------------------"
             $cmd --version
             ;;
-        go)
+        git)
+            echo "-----------------------------"
+            echo "FORMULA: $cmd -v | --version "
+            echo "-----------------------------"
+            $cmd version
+            ;;
+        hugo|go)
             echo "----------------------------"
             echo "FORMULA: $cmd version"
             echo "----------------------------"
             $cmd version
             ;;
+        java)
+            echo "-------------------------------FORMULA-----------------------------------"
+            echo "-version      print product version to the error stream and exit"
+            echo "--version     print product version to the output stream and exit"
+            echo "-showversion  print product version to the error stream and continue"
+            echo "--show-version"
+            echo "              print product version to the output stream and continue"
+            echo "-------------------------------------------------------------------------"
+            $cmd --version
+            ;;
+        clojure|clj)
+            echo "----------------------FORMULA------------------------"
+            echo "--version  Print the version to stdout and exit"
+            echo "-version   Print the version to stderr and exit"
+            echo "-----------------------------------------------------"
+            $cmd --version
+            ;;
         rustc)
-            echo "----------------------------"
+            echo "-----------------------------"
             echo "FORMULA: $cmd -V | --version"
-            echo "----------------------------"
+            echo "-----------------------------"
+            $cmd --version
+            ;;
+        rust-analyzer)
+            echo "---------------------------------"
+            echo "FORMULA: $cmd --version"
+            echo "---------------------------------"
             $cmd --version
             ;;
         solana)
-            echo "----------------------------"
+            echo "-----------------------------"
             echo "FORMULA: $cmd -V | --version"
-            echo "----------------------------"
+            echo "-----------------------------"
             $cmd --version
             ;;
         *)
