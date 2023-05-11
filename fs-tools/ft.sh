5a76225df35a90da7bbac08828b2c9992bef1dc5
Linerre <leon.errelin@outlook.com>
2023-05-11 17:15:34 +0800

    Make ft work for FT_DDMMYYYY files

--- fs-tools/ft.sh
+++ fs-tools/ft.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+#
+# File name: `Financial Times DD-MM-YY.pdf or FT_DDMMYYYY.pdf'l
+#
 # set -x
 
 # -------------------- SET UP ---------------------
@@ -6,11 +9,6 @@ read -p "Full Year (4-digit)? > " year
 if [[ ${#year} -eq 0 ]]; then
     # Default to this year
     year=$(date +"%Y")
-fi
-read -p "World or UK? [e/u] > " category
-if [[ ${#category} -eq 0 ]]; then
-    # Default to UK
-    category="u"
 fi
 
 printf "Archive news of the year %s\n" $year
@@ -20,84 +18,78 @@ dc="$HOME/Documents"
 back=$(pwd)
 
 # -------------------- LOGIC ---------------------
+_mov() {
+    if [[ -f "$2" ]]; then
+        echo "$2 exists; skipping ..."
+    else
+        mv "$1" "$2"
+    fi
+}
+
 move_ft() {
     cd $dw
     cd "Telegram Desktop"
     echo "Moved into: " $(pwd)
-    # File name: `Financial Times DD-MM-YY.pdf or FT_DDMMYYYY.pdf'
-    case $category in
-        "e")
-            local fts=$( ls | grep -e "^Financial" | grep -wv -e "UK" | awk '{print $3}' )
-            local pfx="Financial Times"
-            local sfx="eu"
-            ;;
-        "u")
-            local fts=$( ls | grep -e "^Financial" | grep -w -e "UK" | awk '{print $4}')
-            local pfx="Financial Times UK"
-            local sfx="uk"
-            ;;
-        *)
-            echo "Expect category of w/u, given" $category
-            return -10
-    esac
+    local fts=$( ls | grep -e "^FT" )
+    local pfx="Financial Times"
+    local sfx="eu"
 
     local y=${year}
     for ft in $fts; do
-        local d=${ft:0:2}
-        local m=${ft:3:2}
+        local d=${ft:3:2}
+        local m=${ft:5:2}
         # Create the month dir as needed
         local des=${dc}/FT/${y}/${m}
         [[ -d $des ]] || mkdir $des
-        local src="${pfx} ${ft}"
         local nn="${d}-${sfx}.pdf"
         case $m in
-            "01" )
-                printf "Moving January issue %s\n" "$src"
-                mv "${src}" "${des}/${nn}"
+            01 )
+                printf "Moving January issue %s\n" "$ft"
+                _mov "${ft}" "${des}/${nn}"
                 ;;
-            "02" )
-                printf "Moving Feburary issue %s\n" "$src"
-                mv "${src}" "${des}/${nn}"
+            02 )
+                printf "Moving Feburary issue %s\n" "$ft"
+                _mov "${ft}" "${des}/${nn}"
                 ;;
-            "03" )
-                printf "Moving March issue %s\n" "$src"
-                mv "${src}" "${des}/${nn}"
+            03 )
+                printf "Moving March issue %s\n" "$ft"
+                _mov "${ft}" "${des}/${nn}"
                 ;;
-            "04" )
-                printf "Moving April issue %s\n" "$src"
-                mv "${src}" "${des}/${nn}"
+            04 )
+                printf "Moving April issue %s\n" "$ft"
+                _mov "${ft}" "${des}/${nn}"
                 ;;
-            "05" )
-                printf "Moving May issue %s\n" "$src"
-                mv "${src}" "${des}/${nn}"
+            05 )
+                printf "Moving May issue %s\n" "$ft"
+                _mov "${ft}" "${des}/${nn}"
                 ;;
-            "05" )
-                printf "Moving June issue %s\n" "$src"
-                mv "${src}" "${des}/${nn}"
+            05 )
+                printf "Moving June issue %s\n" "$ft"
+                _mov "${ft}" "${des}/${nn}"
                 ;;
-            "07" )
-                printf "Moving July issue %s\n" "$src"
-                mv "${src}" "${des}/${nn}"
+            07 )
+                printf "Moving July issue %s\n" "$ft"
+                _mov "${ft}" "${des}/${nn}"
                 ;;
-            "08" )
-                printf "Moving August issue %s\n" "$src"
-                mv "${src}" "${des}/${nn}"
+            08 )
+                printf "Moving August issue %s\n" "$ft"
+                _mov "${ft}" "${des}/${nn}"
                 ;;
-            "09" )
-                printf "Moving Semptember issue %s\n" "$src"
-                mv "${src}" "${des}/${nn}"
+            09 )
+                printf "Moving Semptember issue %s\n" "$ft"
+                _mov "${ft}" "${des}/${nn}"
                 ;;
-            "10" )
-                printf "Moving October issue %s\n" "$src"
-                mv "${src}" "${des}/${nn}"
+            10 )
+                printf "Moving October issue %s\n" "$ft"
+                _mov "${ft}" "${des}/${nn}"
                 ;;
-            "11" )
-                printf "Moving November issue %s\n" "$src"
-                mv "${src}" "${des}/${nn}"
+            11 )
+                printf "Moving November issue %s\n" "$ft"
+                _mov "${ft}" "${des}/${nn}"
                 ;;
-            "12" )
-                printf "Moving December issue %s\n" "$src"
-                mv "${src}" "${des}/${nn}"
+            12 )
+                printf "Moving December issue %s\n" "$ft"
+                _mov "${ft}" "${des}/${nn}"
                 ;;
             * )
                 printf "Found strange month: %s\n" $m
@@ -112,5 +104,5 @@ move_ft() {
 move_ft
 
 # ----------------------- CLEAN ----------------------
-unset dw dc category year back
+unset dw dc year back
 unset -f move_ft
