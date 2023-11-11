3973ae72a08c30155e5f72a28ad1b2bf049b3468
Linerre <leon.errelin@outlook.com>
2023-11-11 18:03:38 +1100

    Finish downloading and renaming part

--- fs-tools/edl.sh
+++ fs-tools/edl.sh
@@ -4,21 +4,68 @@
 # URL usually looks like this
 # https://<domain>/path/to/title.pdf
 
-coll="$HOME/Documents/books/"
+DEST="$HOME/Documents/books"
+TEMPDIR="${DEST}/temp"
+
+# Check if a URL is provided
+if [ -z "$1" ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+
+# Set up an exit trap to delete the temporary directory on script exit
+cleanup() {
+  rm -r "$temp_dir"
+}
+
+# Trap exit signals and execute the cleanup function
+# trap cleanup EXIT
 
 download() {
-    local URL=$1
-    echo "To download from: $URL"
+
+    # Looks for ``.*filename="(anything)'' and extract ``anything'' part
+    filename=$(curl -sI "$1" | grep -i 'content-disposition' | sed -n 's/.*filename="\([^"]*\).*/\1/p')
+
+    if [ -z "$filename" ]; then
+        echo "Could not determine the remote filename."
+        exit 1
+    fi
+
+    # Extract title from the filename
+    title=$(echo "$filename" | sed "s/\s*([0-9]*)\s*//" | awk -F ' - ' '{print $2}')
+    # -d -t (use template)
+
+
+    # This `./' is not allowed and mktemp creates a dir undert /tmp by default
+    # local temp_dir=$(mktemp --directory -t ./black.XXXXXXX)
+
     #-OJLw
     # -s --silent
     # -S --show-error
-    curl --write '%{filename_effective}\n' \
-         --silent \
-         --show-error \
-         --output-dir "$coll" \
+    # -- output-dir
+    # --write '%{filename_effective}\n' \
+    echo "== Start downloading ..."
+    echo "== filename: $filename"
+    local URL="$1"
+    curl --output-dir "$TEMPDIR" \
          --remote-name \
          --remote-header-name \
          --location ${URL}
+
+    cd "$TEMPDIR"
+    echo "== Now in $TEMPDIR"
+    echo "== Renaming $filename"
+    mv "$filename" "$title"
+    echo "== New filename $title"
+
+    echo "Copying the downloaded file to $DEST"
+    mv "$title" "${DEST}"/
+    echo "File saved as ${DEST}/${title}"
+
+    cd -
+    echo "DONE"
+    exit 0
 }
 
 download $1
